09980384a0c495aa2161abd0b0a53f46435c8ea2
hongphuc14 <nthphuc21@clc.fitus.edu.vn>
2023-11-15 02:13:17 +0700

    update all report details

--- public/js/CanBo/PhuongQuan/detailReport.js
+++ public/js/CanBo/PhuongQuan/detailReport.js
@@ -77,7 +77,7 @@ $(document).ready(function() {
     let ads_report = storedAdsReport ? JSON.parse(storedAdsReport) : [];
     console.log("ads_report:", ads_report );
 
-    let info1 = ads_report.find(item => item[0] == id_report);
+    let info = ads_report.find(item => item[0] == id_report);
     
     function formatDate(inputDate) {
       const date = new Date(inputDate);
@@ -88,21 +88,21 @@ $(document).ready(function() {
       return `${year}-${month}-${day}`;
     }
     
-    if (info1.length > 0) {
-      $('#id_ads_report').val(info1[0]);
-      $('#id_ads').val(info1[3]);
-      $('#report_type').val(report_type[info1[4] - 1][1]);
-      $('#name').val(info1[5]);
-      $('#email').val(info1[6]);
-      $('#phone').val(info1[7]);
-      $('#report').val(info1[8]);
-      $('#time').val(formatDate(info1[11]));
-      $('#officer').val(info1[1]);
-      $('#office').val(info1[2] === 1 ? "Quận" : (info1[2] === 2 ? "Phường" : ""));
-      $('#status').val(info1[12] === 1 ? "Đã xét duyệt" : "Chưa xét duyệt");
-      $('#method').val(info1[13]);
-      $('.image-report-1').attr('src', `../../../../public/image/${info1[9]}`);
-      $('.image-report-2').attr('src', `../../../../public/image/${info1[10]}`);
+    if (info.length > 0) {
+      $('#id_ads_report').val(info[0]);
+      $('#id_ads').val(info[3]);
+      $('#report_type').val(report_type[info[4] - 1][1]);
+      $('#name').val(info[5]);
+      $('#email').val(info[6]);
+      $('#phone').val(info[7]);
+      $('#report').val(info[8]);
+      $('#time').val(formatDate(info[11]));
+      $('#officer').val(info[1]);
+      $('#office').val(info[2] === 1 ? "Quận" : (info[2] === 2 ? "Phường" : ""));
+      $('#status').val(info[12] === 1 ? "Đã xét duyệt" : "Chưa xét duyệt");
+      $('#method').val(info[13]);
+      $('.image-report-1').attr('src', `../../../../public/image/${info[9]}`);
+      $('.image-report-2').attr('src', `../../../../public/image/${info[10]}`);
     }
 
     $('.style1-button').on('click', function() {
@@ -118,14 +118,273 @@ $(document).ready(function() {
     });
   }
     
-  else if (table == "adsloc")
-    $(".title-page p").text("Quản lý báo cáo vi phạm / Chi tiết báo cáo điểm đặt")
-  else if (table == "loc")
+  else if (table == "adsloc"){
+    $(".title-page p").text("Quản lý báo cáo vi phạm / Chi tiết báo cáo điểm đặt");
+    const pos = document.querySelector('.form-pos');
+    const formHtml = `
+    <form class="row g-3 form-ads-create-details">
+      <div class="col-md-12">
+          <label class="form-label" style="font-weight: 600; font-size: 1rem;">Chi tiết báo cáo điểm đặt vi phạm</label>
+      </div>
+      <div class="col-md-6">
+          <label for="id_ads_loc_report" class="form-label">ID Báo cáo</label>
+          <input type="number" class="form-control input-details" id="id_ads_loc_report" readonly>
+      </div>
+      <div class="col-md-6">
+        <label for="id_ads_loc" class="form-label">ID Điểm đặt</label>
+        <input type="number" class="form-control input-details" id="id_ads_loc" readonly>
+      </div>
+
+      <div class="col-md-6">
+        <label for="report_type" class="form-label">Loại hình báo cáo</label>
+        <input type="text" class="form-control input-details" id="report_type" readonly>
+      </div>
+      <div class="col-md-6">
+        <label for="name" class="form-label">Họ tên người báo cáo</label>
+        <input type="text" class="form-control input-details" id="name" readonly>
+      </div>
+
+      <div class="col-md-6">
+          <label for="email" class="form-label">Email người báo cáo</label>
+          <input type="email" class="form-control input-details" id="email" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="phone" class="form-label">Số điện thoại người báo cáo</label>
+          <input type="phone" class="form-control input-details" id="phone" readonly>
+      </div>
+
+      <div class="col-md-12">
+          <label for="report" class="form-label">Nội dung báo cáo</label>
+          <textarea type="text" class="form-control input-details" id="report" readonly></textarea>
+      </div>
+
+      <div class="col-md-6">
+          <label for="time" class="form-label">Thời điểm gửi</label>
+          <input type="date" class="form-control input-details" id="time" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="officer" class="form-label">Người xử lý</label>
+          <input type="text" class="form-control input-details" id="officer" readonly>
+      </div>
+
+      <div class="col-md-6">
+          <label for="office" class="form-label">Đơn vị xử lý</label>
+          <input type="text" class="form-control input-details" id="office" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="status" class="form-label">Trạng thái xử lý</label>
+          <input type="text" class="form-control input-details" id="status" readonly>
+      </div>
+
+      <div class="col-md-12">
+          <label for="method" class="form-label">Cách thức xử lý</label>
+          <textarea class="form-control edit-input" id="method"></textarea>
+      </div>
+    </form>
+    `;
+    
+    pos.insertAdjacentHTML('afterend', formHtml);
+
+    const storedAdsLocReport = localStorage.getItem('ads_loc_report');
+    let ads_loc_report = storedAdsLocReport ? JSON.parse(storedAdsLocReport) : [];
+    console.log("ads_loc_report:", ads_loc_report );
+
+    let info = ads_loc_report.find(item => item[0] == id_report);
+    
+    function formatDate(inputDate) {
+      const date = new Date(inputDate);
+      const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+      const day = date.getDate().toString().padStart(2, '0');
+      const year = date.getFullYear();
+
+      return `${year}-${month}-${day}`;
+    }
+    
+    if (info.length > 0) {
+      $('#id_ads_loc_report').val(info[0]);
+      $('#id_ads_loc').val(info[3]);
+      $('#report_type').val(report_type[info[4] - 1][1]);
+      $('#name').val(info[5]);
+      $('#email').val(info[6]);
+      $('#phone').val(info[7]);
+      $('#report').val(info[8]);
+      $('#time').val(formatDate(info[11]));
+      $('#officer').val(info[1]);
+      $('#office').val(info[2] === 1 ? "Quận" : (info[2] === 2 ? "Phường" : ""));
+      $('#status').val(info[12] === 1 ? "Đã xét duyệt" : "Chưa xét duyệt");
+      $('#method').val(info[13]);
+      $('.image-report-1').attr('src', `../../../../public/image/${info[9]}`);
+      $('.image-report-2').attr('src', `../../../../public/image/${info[10]}`);
+    }
+
+    $('.style1-button').on('click', function() {
+      const updatedMethod = $('#method').val();
+      const indexToUpdate = ads_loc_report.findIndex(item => item[0] == id_report);
+
+      ads_loc_report[indexToUpdate][13] = updatedMethod;
+      ads_loc_report[indexToUpdate][1] = storedEmail;
+      ads_loc_report[indexToUpdate][2] = 2;
+      ads_loc_report[indexToUpdate][12] = 1;
+      localStorage.setItem('ads_loc_report', JSON.stringify(ads_loc_report)); 
+      location.reload();
+    });
+  }
+    
+  else if (table == "loc"){
     $(".title-page p").text("Quản lý báo cáo vi phạm / Chi tiết báo cáo địa điểm")
+    const pos = document.querySelector('.form-pos');
+    const formHtml = `
+    <form class="row g-3 form-ads-create-details">
+      <div class="col-md-12">
+          <label class="form-label" style="font-weight: 600; font-size: 1rem;">Chi tiết báo cáo địa điểm vi phạm</label>
+      </div>
+      <div class="col-md-12">
+          <label for="id_loc_report" class="form-label">ID Báo cáo</label>
+          <input type="number" class="form-control input-details" id="id_loc_report" readonly>
+      </div>
+      <div class="col-md-12">
+        <label for="address" class="form-label">Địa chỉ</label>
+        <textarea type="number" class="form-control input-details" id="address" readonly></textarea>
+      </div>
 
-  
+      <div class="col-md-6">
+        <label for="report_type" class="form-label">Loại hình báo cáo</label>
+        <input type="text" class="form-control input-details" id="report_type" readonly>
+      </div>
+      <div class="col-md-6">
+        <label for="name" class="form-label">Họ tên người báo cáo</label>
+        <input type="text" class="form-control input-details" id="name" readonly>
+      </div>
 
-        
+      <div class="col-md-6">
+          <label for="email" class="form-label">Email người báo cáo</label>
+          <input type="email" class="form-control input-details" id="email" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="phone" class="form-label">Số điện thoại người báo cáo</label>
+          <input type="phone" class="form-control input-details" id="phone" readonly>
+      </div>
+
+      <div class="col-md-12">
+          <label for="report" class="form-label">Nội dung báo cáo</label>
+          <textarea type="text" class="form-control input-details" id="report" readonly></textarea>
+      </div>
+
+      <div class="col-md-6">
+          <label for="time" class="form-label">Thời điểm gửi</label>
+          <input type="date" class="form-control input-details" id="time" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="officer" class="form-label">Người xử lý</label>
+          <input type="text" class="form-control input-details" id="officer" readonly>
+      </div>
+
+      <div class="col-md-6">
+          <label for="office" class="form-label">Đơn vị xử lý</label>
+          <input type="text" class="form-control input-details" id="office" readonly>
+      </div>
+      <div class="col-md-6">
+          <label for="status" class="form-label">Trạng thái xử lý</label>
+          <input type="text" class="form-control input-details" id="status" readonly>
+      </div>
+
+      <div class="col-md-12">
+          <label for="method" class="form-label">Cách thức xử lý</label>
+          <textarea class="form-control edit-input" id="method"></textarea>
+      </div>
+    </form>
+    `;
+    
+    pos.insertAdjacentHTML('afterend', formHtml);
+
+    const storedLocReport = localStorage.getItem('loc_report');
+    let loc_report = storedLocReport ? JSON.parse(storedLocReport) : [];
+    console.log("loc_report:", loc_report );
+
+    let info = loc_report.find(item => item[0] == id_report);
+    
+    function formatDate(inputDate) {
+      const date = new Date(inputDate);
+      const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+      const day = date.getDate().toString().padStart(2, '0');
+      const year = date.getFullYear();
+
+      return `${year}-${month}-${day}`;
+    }
+    function getAddressFromCoordinates(latitude, longitude) {
+      return new Promise((resolve, reject) => {
+        const accessToken = 'pk.eyJ1IjoicG1saW5oMjEiLCJhIjoiY2xueXVlb2ZsMDFrZTJsczMxcWhjbmo5cSJ9.uNguqPwdXkMJwLhu9Cwt6w';
+        const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${accessToken}`;
+    
+        $.ajax({
+          url: url,
+          method: 'GET',
+          success: function (data) {
+            if (data.features && data.features.length > 0) {
+              if (data.features[0].place_name) {
+                const fullAddress = data.features[0].place_name;
+                const addressComponents = fullAddress.split(', ');
+                const simplifiedAddress = addressComponents.slice(0, 2).join(', ');
+    
+                resolve(simplifiedAddress);
+              } else {
+                console.error('Dữ liệu không chứa thuộc tính "place_name".', data.features[0]);
+                reject('Địa chỉ không khả dụng');
+              }
+            } else {
+              console.error('Dữ liệu không đúng định dạng hoặc không chứa thuộc tính "features".', data);
+              reject('Địa chỉ không khả dụng');
+            }
+          },
+          error: function (error) {
+            console.error('Error fetching address:', error);
+            reject('Địa chỉ không khả dụng');
+          }
+        });
+      });
+    }
+    
+    function updateAddressField(latitude, longitude) {
+      getAddressFromCoordinates(latitude, longitude)
+        .then((address) => {
+          $('#address').val(address);
+        })
+        .catch((error) => {
+          console.error('Có lỗi xảy ra:', error);
+          // Xử lý lỗi nếu cần
+        });
+    }
+    
+    if (info.length > 0) {
+      $('#id_loc_report').val(info[0]);
+      updateAddressField(info[3], info[4]);
+      $('#report_type').val(report_type[info[5] - 1][1]);
+      $('#name').val(info[6]);
+      $('#email').val(info[7]);
+      $('#phone').val(info[8]);
+      $('#report').val(info[9]);
+      $('#time').val(formatDate(info[12]));
+      $('#officer').val(info[1]);
+      $('#office').val(info[2] === 1 ? "Quận" : (info[2] === 2 ? "Phường" : ""));
+      $('#status').val(info[13] === 1 ? "Đã xét duyệt" : "Chưa xét duyệt");
+      $('#method').val(info[14]);
+      $('.image-report-1').attr('src', `../../../../public/image/${info[10]}`);
+      $('.image-report-2').attr('src', `../../../../public/image/${info[11]}`);
+    }
+
+    $('.style1-button').on('click', function() {
+      const updatedMethod = $('#method').val();
+      const indexToUpdate = loc_report.findIndex(item => item[0] == id_report);
+
+      loc_report[indexToUpdate][14] = updatedMethod;
+      loc_report[indexToUpdate][1] = storedEmail;
+      loc_report[indexToUpdate][2] = 2;
+      loc_report[indexToUpdate][12] = 1;
+      localStorage.setItem('loc_report', JSON.stringify(loc_report)); 
+      location.reload();
+    });
+  }
+      
   const manageButton = $('#manage');
   const manageMenu = $('#manage .manage-menu');
 
