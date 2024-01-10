910197f3320e24e083101003487f6dcd0922eacf
kreema1602 <100921862+kreema1602@users.noreply.github.com>
2024-01-10 23:38:40 +0700

    Add request module and import it in app.js and nguoidanController.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 require("dotenv").config();
 
+const request = require('request');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
 
