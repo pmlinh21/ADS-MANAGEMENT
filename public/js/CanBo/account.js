f4684bb8a76d51baee36cc1fa63296060694d601
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2024-01-12 09:06:52 +0700

    update navbar in sở

--- public/js/CanBo/account.js
+++ public/js/CanBo/account.js
@@ -402,21 +402,4 @@ $(document).ready(function() {
             });
         });
     });
-
-    const manageButton = $('#manage');
-    const manageMenu = $('#manage .manage-menu');
-    manageButton.hover(
-    function () {
-        $(this).addClass('li-hover');
-        $('#manage .nav-link').addClass('nav-link-hover');
-        manageMenu.show();
-        $('.black-bg').show()
-      },
-      function () {
-        $(this).removeClass('li-hover');
-        $('#manage .nav-link').removeClass('nav-link-hover');
-        manageMenu.hide();
-        $('.black-bg').hide()
-      }    
-    );
 })
